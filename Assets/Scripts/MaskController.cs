fdacfbbd0a8ca5a60aee01e0692f697c8d7a7d42
Rémy Zins <rzins-tmp@c2r1s5.marseille.42.school>
2026-01-30 08:30:29 +0100

    mask spawns away from the player

--- Assets/Scripts/MaskController.cs
+++ Assets/Scripts/MaskController.cs
@@ -6,8 +6,12 @@ using Random = UnityEngine.Random;
 public class MaskController : MonoBehaviour
 {
     public GameObject maskPrefab;
-    public Vector2 mapMinBounds = new Vector2(-10, -10);
-    public Vector2 mapMaxBounds = new Vector2(10, 10);
+    private PlayerController playerController;
+    public GameObject player;
+    // float width = GameObject.Find("Map").GetComponent<SpriteRenderer>().bounds.size.x;
+    // float height = GameObject.Find("Map").GetComponent<SpriteRenderer>().bounds.size.y;
+    // public Vector2 mapMinBounds = new Vector2(-width, -height);
+    // public Vector2 mapMaxBounds = new Vector2(width, height);
     
     private int spawnMasksCount = 0;
     private const int MAX_SPAWN_MASKS = 3;
@@ -21,15 +25,39 @@ public class MaskController : MonoBehaviour
     {
         // Optional: Check if masks need respawning
     }
+
+    Vector2 getRandomPosition()
+    {
+        float width = GameObject.Find("Map").GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        float height = GameObject.Find("Map").GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        // Transform playerTransform = GameObject.FindWithTag("Player").transform;
+        // Vector3 playerPosition = playerTransform.position;
+        Vector3 playerPosition = GameObject.FindWithTag("Player").transform.position;
+        Vector2 playerPos2D = new Vector2(playerPosition.x, playerPosition.y);
+        Vector2 randomPosition = new Vector2(
+            Random.Range(-width, width),
+            Random.Range(-height, height)
+        );
+        while (Vector2.Distance(playerPos2D, randomPosition) < 10)
+        {
+            randomPosition = new Vector2(
+                Random.Range(-width, width),
+                Random.Range(-height, height)
+            );
+        }
+        return randomPosition;
+    }
     
     void SpawnMask()
     {
         if (spawnMasksCount >= MAX_SPAWN_MASKS) return;
         
-        Vector2 randomPosition = new Vector2(
-            Random.Range(mapMinBounds.x, mapMaxBounds.x),
-            Random.Range(mapMinBounds.y, mapMaxBounds.y)
-        );
+        // Vector2 randomPosition = new Vector2(
+        //     Random.Range(mapMinBounds.x, mapMaxBounds.x),
+        //     Random.Range(mapMinBounds.y, mapMaxBounds.y)
+        // );
+
+        Vector2 randomPosition = getRandomPosition();
         
         if (maskPrefab == null) return;
         GameObject newMask = Instantiate(maskPrefab, randomPosition, Quaternion.identity);
